b7d15ed2ab302f54f8a109727b27007bd11744c5
b3m2a1 <b3m2a1@gmail.com>
2020-08-12 19:53:06 -0400

    n_quanta was too low to get things right...

--- VPT2/Representations.py
+++ VPT2/Representations.py
@@ -30,6 +30,8 @@ class SubHamiltonian:
         ndims = int(np.prod(dims))
         idx = (n, m)
 
+        # There are two possible modes for this pulling individual elements or pulling blocks
+        # the block pulling is quite a bit faster, so we try to detect first off if we want to do that
         pull_elements = True
         if isinstance(n, np.ndarray) and isinstance(m, np.ndarray):
             if len(n.shape) > 1 and len(m.shape) > 1:
@@ -41,6 +43,7 @@ class SubHamiltonian:
                 if pull_elements:
                     e1 = len(idx[0])
                     pull_elements = all(len(x) == e1 for x in idx)
+        # We figure out the row spec
         if not isinstance(n, int):
             if isinstance(n, np.ndarray):
                 n = n.flatten()
@@ -49,6 +52,7 @@ class SubHamiltonian:
             n = np.arange(ndims)[n]
         else:
             n = [n]
+        # Then the column spec
         if not isinstance(m, int):
             if isinstance(m, np.ndarray):
                 m = m.flatten()
@@ -57,26 +61,31 @@ class SubHamiltonian:
             m = np.arange(ndims)[m]
         else:
             m = [m]
+
         if pull_elements:
+            # If we're just pulling elements we need only unravel those indices
             n = np.unravel_index(n, dims)
             m = np.unravel_index(m, dims)
         else:
+            # If we're pulling blocks we need to compute the product of the row
+            #  and column indices to get the total index spec
             blocks = np.array(list(ip.product(n, m)))
             n = np.unravel_index(blocks[:, 0], dims)
             m = np.unravel_index(blocks[:, 1], dims)
+
+        # we define a temporary helper to pad repeat the indices if necessary
         def pad_lens(a, b):
             if isinstance(a, (int, np.integer)) and not isinstance(b, (int, np.integer)):
                 a = np.full((len(b),), a)
             if isinstance(b, (int, np.integer)) and not isinstance(a, (int, np.integer)):
                 b = np.full((len(a),), b)
-            return a ,b
-        i = tuple(pad_lens(a, b) for a ,b in zip(n ,m))
+            return a, b
+        i = tuple(pad_lens(a, b) for a, b in zip(n, m))
         els = self.compute(i)
-        if pull_elements:
-            return els
-        else:
+        if not pull_elements:
             shp = (len(np.unique(blocks[:, 0])), len(np.unique(blocks[:, 1])))
-            return els.reshape(shp).squeeze()
+            els = els.reshape(shp).squeeze()
+        return els
 
     def __getitem__(self, item):
         if not isinstance(item, tuple):
