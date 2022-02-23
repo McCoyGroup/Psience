6b13884d1ebdd92c10794a21748e3dd54f8e7577
b3m2a1 <b3m2a1@gmail.com>
2022-02-23 13:18:33 -0800

    Cleaned up G-matrix and V' terms

---
+++ Psience/Data/Helpers.py
@@ -0,0 +1,80 @@
+
+import numpy as np
+try:
+    import sympy as sym
+except ImportError:
+    class SympyNotLoadedError(ImportError):
+        ...
+    class SympyNotLoadedStub:
+        def __getattr__(self, item):
+            raise SympyNotLoadedError("Analytic models depend on sympy, which couldn't be imported")
+    sym = SympyNotLoadedStub()
+
+class SymbolicCaller:
+    def __init__(self, sym):
+        self.sym = sym
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self.sym(item)
+        else:
+            return self.sym(*item)
+
+class AnaylticModelBase:
+    @classmethod
+    def take_derivs(cls, expr, vars):
+        if isinstance(expr, list):
+            return [cls.take_derivs(e, vars) for e in expr]
+        else:
+            return [sym.diff(expr, v) for v in vars]
+
+    @classmethod
+    def eval_exprs(cls, expr, subs):
+        if isinstance(expr, list):
+            return [cls.eval_exprs(e, subs) for e in expr]
+        elif isinstance(expr, (int, float, np.integer, np.floating)):
+            return expr
+        else:
+            return expr.subs(subs)
+
+    @classmethod
+    def symbol_list(cls, names, instance=None):
+        instance_tag = "" if instance is None else str(instance)
+        return sym.symbols(" ".join(n+instance_tag for n in names))
+    @staticmethod
+    def symbolic_m(i):
+        return sym.Symbol('m[{i}]'.format(i=i), positive=True)
+    @staticmethod
+    def symbolic_r(i, j):
+        if j < i:
+            i, j = j, i
+        return sym.Symbol('r[{i},{j}]'.format(i=i, j=j), positive=True)
+    @staticmethod
+    def symbolic_a(i, j, k):
+        if k < i:
+            i, k = k, i
+        return sym.Symbol('a[{i},{j},{k}]'.format(i=i, j=j, k=k), real=True)
+    @staticmethod
+    def symbolic_t(i, j, k, l):
+        if l < i:
+            i, j, k, l = l, k, j, i
+        return sym.Symbol('t[{i},{j},{k},{l}]'.format(i=i, j=j, k=k, l=l), real=True)
+    @staticmethod
+    def symbolic_y(i, j, k, l):
+        return sym.Symbol('y[{i},{j},{k},{l}]'.format(i=i, j=j, k=k, l=l), real=True)
+
+    @staticmethod
+    def reindex_symbol(symbol, mapping):
+        name = symbol.name
+        for k,v in mapping.items():
+            name = name.replace(str(k), str(k)+"$")
+        for k,v in mapping.items():
+            name = name.replace(str(k)+"$", str(v))
+        return sym.Symbol(name, positive=symbol.is_positive, real=symbol.is_real)
+
+    @classmethod
+    def lam(cls, i, j, k):
+        r12 = cls.symbolic_r(i, j)
+        r23 = cls.symbolic_r(j, k)
+        a = cls.symbolic_a(i, j, k)
+        return 1/sym.sin(a)*(1/r12 - sym.cos(a)/r23)
