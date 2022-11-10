4cf8d9a2a2e9be3d1ed5d3693e77aa67be3ed9a9
b3m2a1 <b3m2a1@gmail.com>
2022-11-09 18:41:29 -0600

    Adding in DGB support for expansion potentials

--- Psience/DGB/DGB.py
+++ Psience/DGB/DGB.py
@@ -1,5 +1,6 @@
 import numpy as np, scipy as sp, itertools, functools
 from McUtils.Zachary import RBFDInterpolator
+from McUtils.Combinatorics import StirlingS1
 
 __all__ =  [
     "DGB"
@@ -10,12 +11,18 @@ class DGB:
     """
 
     """
-    def __init__(self, centers, potential_function,
+    def __init__(self,
+                 centers,
+                 potential_function,
                  alphas=1,
-                 clustering_radius=None,
-                 quadrature_degree=4
+                 clustering_radius=.075,
+                 quadrature_degree=4,
+                 expansion_degree=None,
+                 expansion_type='multicenter'
     ):
         centers = np.asanyarray(centers)
+        if centers.ndim == 1:
+            centers = centers[:, np.newaxis]
         if clustering_radius is not None and clustering_radius >= 0:
             centers, _, _ = RBFDInterpolator.decluster_data(centers, np.empty(len(centers)), [], clustering_radius)
         self.centers = centers
@@ -25,6 +32,8 @@ class DGB:
         self.alphas = np.asanyarray(alphas)
         self.potential_function = potential_function
         self.quadrature_degree = quadrature_degree
+        self.expansion_degree = expansion_degree
+        self.expansion_type = expansion_type
         self._S, self._T, self._V = None, None, None
 
     @property
@@ -102,29 +111,158 @@ class DGB:
     def morse_integral1d(cls, centers, alpha, de, a):
         # Centers: (n, n, 2)
         # Alphas: (n, n, 2)
-
         ...
 
+    # @classmethod
+    # def polyint_1D(cls, centers, alphas, order):
+    #     ...
+
+
+    @classmethod
+    def polyint_1D(cls, centers, alphas, n):
+        if n == 0:
+            return np.ones(alphas.shape[:-1])
+        c = centers*np.sqrt(alphas)
+        prefac = cls.polyint_1D_prefac(centers, alphas, n)
+        expr = cls.polyint_1D_poly_eval(n, c)
+        # with np.printoptions(linewidth=1e8):
+        #     print(prefac)
+        #     print(expr)
+
+        return prefac * expr
+
+    # a complicated set of functions to handle integrating a monomial (x^n) between
+    # two Gaussians in a Taylor series type of way...
+    @classmethod
+    def polyint_1D_prefac(cls, centers, alphas, n):
+        return np.sqrt( np.pi / (4 * alphas**(n+1)) )
+    _stirs = None
+    @classmethod
+    def stirling(cls, n, m):
+        if cls._stirs is None or len(cls._stirs) <= n:
+            cls._stirs = StirlingS1(2*n)
+        return cls._stirs[n, m]
+    _poly_cache = {}
+    @classmethod
+    def polyint_1D_poly_coeff_gen(cls, n):
+        # cls.stirling(4, 1),
+        # with np.printoptions(linewidth=1e8):
+        #     print(cls._stirs)
+        # raise Exception( ... )
+        if n not in cls._poly_cache:
+            scaling = np.prod(2*(1+np.arange(n))) * (np.power(2, n-1) if n > 0 else 1/2 )
+            terms = [(-1) ** i * cls.stirling(2*n, i) for i in range(1, 2*n+1)]
+            cls._poly_cache[n] = (scaling, terms)
+        return cls._poly_cache[n]
+    @classmethod
+    def polyint_1D_coeff(cls, i, n):
+        scaling, terms = cls.polyint_1D_poly_coeff_gen(i)
+        return np.dot(n**np.arange(1, 2*i+1), terms) / scaling
+    @classmethod
+    def polyint_1D_coeffs(cls, i):
+        o = i % 2
+        return [ # I could make this faster but I don't _think_ it'll cost me all that much
+                cls.polyint_1D_coeff(
+                    (i - (n-1-o) )//2,
+                    n
+                )
+                for n in range(1+o, 1+i, 2)
+            ] + [2]
+    @classmethod
+    def polyint_1D_poly_eval(cls, i, c):
+        o = i%2
+        return np.dot(
+            c[:, :, np.newaxis]**np.arange(o, i+1, 2)[np.newaxis, :],
+            cls.polyint_1D_coeffs(i)
+        )
+    @classmethod
+    def simple_poly_int(cls, alphas, n):
+        """
+        1/2 (1 + (-1)^n) \[Alpha]^(-(1/2) - n/2) Gamma[(1 + n)/2]
+        """
+        from scipy.special import gamma
+        return 1/2 * (1 + (-1)**n) / np.sqrt(alphas**(n+1)) * gamma((n+1)/2)
+
+    def expansion_integrate(self, deriv_order=2, expansion_type='multicenter'):
+        centers, alphas = self.get_overlap_gaussians()
+        # this prefac allows us to reexpress things in terms of the new Gaussians
+        # with appropriate weighting
+        aprod = self.alphas[:, np.newaxis] * self.alphas[np.newaxis, :]
+        asum = self.alphas[:, np.newaxis] + self.alphas[np.newaxis, :]
+        cdiff = self.centers[:, np.newaxis, :] - self.centers[np.newaxis, :, :]
+        prefac = 4 * np.sqrt(2) / np.sqrt(np.pi) * (aprod**1/4) * np.exp(-aprod/asum * np.sum(cdiff**2, axis=-1))
+        ndim = centers.shape[-1]
+        if expansion_type == 'taylor':
+            zero = np.zeros((1, centers.shape[-1]))
+            derivs = (
+                    [self.potential_function(zero)] +
+                     [self.potential_function(zero, deriv_order=d) for d in range(1, deriv_order+1)]
+            )
+            derivs = [
+                np.broadcast_to(
+                    d[np.newaxis],
+                    alphas.shape + d.shape[1:]
+                )
+                for d in derivs
+            ]
+        else:
+            derivs = [self.potential_function(centers)] + [self.potential_function(centers, deriv_order=d) for d in range(1, deriv_order+1)]
+        caches = [{} for _ in range(ndim)]
+        pot = 0
+        for d in derivs: # add up all independent integral contribs...
+            # iterate over upper triangle coordinates (we'll add bottom contrib by symmetry)
+            inds = itertools.combinations_with_replacement(range(ndim), r=d.ndim-2) if d.ndim > 2 else [()]
+            for idx in inds:
+                # print("--->", idx)
+                count_map = {k: v for k, v in zip(*np.unique(idx, return_counts=True))}
+                contrib = prefac
+                for k in range(ndim): # do each dimension of integral independently
+                    n = count_map.get(k, 0)
+                    if n not in caches[k]:
+                        caches[k][n] = (
+                           self.simple_poly_int(alphas, n)
+                            if expansion_type != 'taylor' else
+                           self.polyint_1D(centers[..., k], alphas, n)
+                        )
+
+                    dcont = d[(slice(None, None, None), slice(None, None, None)) + idx] if len(idx) > 0 else d
+                    # print(n, alphas[0, 0], centers[0, 0])
+                    # with np.printoptions(linewidth=1e8):
+                    #     print(caches[k][n])
+                        # print(dcont/np.math.factorial(n))
+
+                    contrib = contrib * dcont/np.math.factorial(n) * caches[k][n]
+                # handle symmetry?
+                pot += contrib
+        return pot
+
+
     def analytic_integrate(self):
+        raise NotImplementedError("flooped up")
         centers = [np.array(np.meshgrid(x, x)).T for x in self.centers.T]
         alphas = np.array(np.meshgrid(self.alphas, self.alphas)).T
         # raise Exception(alphas.shape)
         return self.potential_function['analytic_integrals'](
-            centers,
+            centers, # ...no
             alphas
         )
 
-    def get_V(self, potential_handler=None, degree=None):
-
+    def get_V(self, potential_handler=None, expansion_degree=None, degree=None):
+        if expansion_degree is None:
+            expansion_degree = self.expansion_degree
         if potential_handler is None:
             if isinstance(self.potential_function, dict):
                 if 'analytic_integrals' in self.potential_function:
                     potential_handler = 'analytic'
+            elif expansion_degree is not None:
+                potential_handler = 'expansion'
             else:
                 potential_handler = 'quad'
 
         if potential_handler == 'quad':
             pot_mat = self.quad_integrate(degree=self.quadrature_degree if degree is None else degree)
+        elif potential_handler == 'expansion':
+            pot_mat = self.expansion_integrate(deriv_order=expansion_degree)
         elif potential_handler == 'analytic':
             pot_mat = self.analytic_integrate()
         else:
@@ -134,11 +272,11 @@ class DGB:
 
     def get_wavefunctions(self):
         H = self.T + self.V
-        # print(np.linalg.cond(self.S))
-        # with np.printoptions(linewidth=1e8):
-        #     print(self.S)
-        #     print(self.T)
-        #     print(self.V)
+        print(np.linalg.cond(self.S))
+        with np.printoptions(linewidth=1e8):
+            # print(self.S)
+            # print(self.T)
+            print(self.V)
 
         try:
             return sp.linalg.eigh(H, self.S)
--- ci/tests/DGBTests.py
+++ ci/tests/DGBTests.py
@@ -1,11 +1,13 @@
 import itertools
 
+import McUtils.Zachary
 from Peeves.TestUtils import *
 from unittest import TestCase
 
 from McUtils.Data import UnitsData, PotentialData
 from McUtils.Zachary import Interpolator
 import McUtils.Plots as plt
+from McUtils.GaussianInterface import GaussianLogReader
 
 from Psience.DGB import *
 from Psience.Molecools import Molecule
@@ -40,7 +42,7 @@ class DGBTests(TestCase):
             .0025
         )
 
-    @debugTest
+    @validationTest
     def test_Morse(self):
         d = 2
         ndivs = [100]*d
@@ -70,3 +72,100 @@ class DGBTests(TestCase):
             ),
             .0025
         )
+
+    @debugTest
+    def test_Interp(self):
+        from McUtils.Zachary import Symbols, RBFDInterpolator
+
+        sym = Symbols('xyz')
+
+        # np.random.seed(3)
+        # fn = sym.morse(sym.x)
+        # pts = np.random.uniform(low=-.5, high=1.2, size=30)
+
+        # test_1D(sym.morse(sym.x), pts)
+        # test_1D(sym.sin(sym.x), pts)
+        # test_1D(sym.morse(sym.x)*sym.sin(sym.x), pts)
+        #
+        # raise Exception(...)
+
+        np.random.seed(3)
+        ndim = 1
+        pts = np.random.uniform(low=-.5, high=1.2, size=(1000, ndim))
+
+        # fn = sym.morse(sym.x) * sym.morse(sym.y) - sym.morse(sym.x) - sym.morse(sym.y)
+
+        w = 2.05; wx = .1; mu = 1
+        de = (w ** 2) / (4 * wx)
+        a = np.sqrt(2 * mu * wx)
+        fn = sym.morse(sym.x, de=de, a=a) + sym.morse(sym.y, de=de, a=a)
+        fn = (1/2*sym.x**2)# + 1/2*sym.y**2)
+        # raise Exception(fn(pts))
+
+        # print(res['AIMDEnergies'].gradients)
+        # interp = RBFDInterpolator.create_function_interpolation(
+        #     pts,
+        #     fn,
+        #     lambda p, f=fn.deriv(order=1): f(p),#.transpose(),
+        #     lambda p, f=fn.deriv(order=2): f(p),#.transpose(2, 0, 1),
+        #     clustering_radius=1e-5,
+        # )
+
+        test_pot = lambda c,fn=fn,deriv_order=None: (
+            fn(c).reshape(c.shape[:-1])
+                if deriv_order is None else
+            fn.deriv(order=deriv_order)(c).reshape(c.shape[:-1] + (ndim,)*deriv_order)
+        )
+
+
+        np.random.seed(0)
+        centers = pts[np.unique(np.random.random_integers(low=0, high=len(pts)-1, size=100))]
+        centers = np.linspace(-1, 1, 50).reshape(-1, 1)
+        ham = DGB(
+            centers,
+            test_pot,
+            alphas=1,
+            clustering_radius=0.1
+        )
+        e, wf = ham.get_wavefunctions()
+        print(e)
+
+        # for k in range(1, 6):
+        #     print(DGB.polyint_1D_coeffs(k))
+        # raise Exception(DGB.polyint_1D_coeffs(4))
+
+        ham = DGB(
+            centers,
+            test_pot,
+            expansion_degree=6,
+            alphas=1
+        )
+        # ham = DGB(
+        #     centers,
+        #     test_pot,
+        #     expansion_degree=2, expansion_type='taylor', alphas=1, clustering_radius=.1)
+        e, wf = ham.get_wavefunctions()
+        print(e)
+
+    @inactiveTest
+    def test_Water(self):
+        with GaussianLogReader(TestManager.test_data('h2o_aimd.log')) as parser:
+            res = parser.parse(['AIMDCoordinates', 'AIMDEnergies'])
+
+        crds = res['AIMDCoordinates'].reshape(-1, 9)
+        e = (res['AIMDEnergies'].energies - np.min(res['AIMDEnergies'].energies)) #* 219474.65
+        # print(res['AIMDEnergies'].gradients)
+        interp = McUtils.Zachary.RBFDInterpolator(
+            crds,
+            e,
+            res['AIMDEnergies'].gradients,
+            res['AIMDEnergies'].hessians,
+            clustering_radius=0
+        )
+
+        ham = DGB(crds, interp, alphas=1, clustering_radius=.1)
+        e, wf = ham.get_wavefunctions()
+
+        print(e)
+
+
