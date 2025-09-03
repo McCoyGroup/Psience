41075871335360f939958fbdc678d92b185a7820
b3m2a1 <b3m2a1@gmail.com>
2025-09-03 09:55:03 -0500

    Move symmetries to McUtils

--- Symmetry.py
+++
@@ -1,9 +0,0 @@
-"""
-A file that only exists to make this directory useable as a way to import Psience too.
-Mostly useful during development
-"""
-
-from .Psience.Symmetry import *
-from .Psience.Symmetry import __all__
-from .Psience.Symmetry import __doc__
-__identifier__ = 'Psience.Psience.Symmetry'
